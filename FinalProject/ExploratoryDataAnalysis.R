4e52ad4130cf23a8c78f55485038574bdd0f5e24
Kathleen <kathleen.cachel@gmail.com>
2019-05-28 20:30:47 +0000

    added EDA for time variables

--- FinalProject/ExploratoryDataAnalysis.R
+++ FinalProject/ExploratoryDataAnalysis.R
@@ -4,9 +4,9 @@ library(corrplot)
 library(dplyr) 
 library(ggthemes)
 #library(stringr) # String manipulation
-#library(anytime)
+library(anytime)
 #library(data.table)
-
+library(lubridate)
 
 TedRaw <- read.csv(file="/cloud/project/FinalProject/ted_main.csv", header=TRUE, sep=",")
 
@@ -124,9 +124,73 @@ eventtop <- head(eventtop,20)
 
 ggplot(data=eventtop, aes(x=event, y=numTalks)) +
   #geom_point(stat = "identity", aes(color = '#2b8cbe'))+
-  geom_bar(stat = "identity", fill = '#2b8cbe')+
+  geom_bar(stat = "identity", fill = '#c51b8a')+
   geom_text(aes(label = numTalks), vjust = 1.6, color = "white", size = 3)+
   labs(x = "Event", y = "Count of TED Talks",
        title = "Talks by 20 most popular Events")+
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+
+
+###############################################
+#EDA for time data:
+###############################################
+#film_date
+film_date <- anydate(TedRaw$film_date)
+TedRaw$film_date_clean <- film_date
+
+
+film_date_clean <- TedRaw %>%
+  group_by(film_date_clean) %>%
+  tally(name ="numTalks") 
+
+ggplot(data = film_date_clean, aes(x = film_date_clean, y = numTalks))+
+  geom_line(color = "#feb24c")+
+  labs(x = "Date", y = "Count of TED Talks",
+       title = "Talks by film date")
+
+##########################
+#published_date
+
+TedRaw$published_date_clean <- anydate(TedRaw$published_date)
+
+published_date_clean <- TedRaw %>%
+  group_by(published_date_clean) %>%
+  tally(name ="numTalks") 
+
+ggplot(data = published_date_clean, aes(x = published_date_clean, y = numTalks))+
+  geom_line(color = "#feb24c")+
+  labs(x = "Date", y = "Count of TED Talks",
+       title = "Talks by published date")
+
+#day of week
+
+TedRaw$published_date_wday <- wday(TedRaw$published_date_clean, label = TRUE)
+
+published_date_wday <- TedRaw %>%
+  group_by(published_date_wday) %>%
+  tally(name ="numTalks") 
+
+ggplot(data=published_date_wday, aes(x=published_date_wday, y=numTalks)) +
+  #geom_point(stat = "identity", aes(color = '#2b8cbe'))+
+  geom_bar(stat = "identity", fill = '#2b8cbe')+
+  geom_text(aes(label = numTalks), vjust = 1.6, color = "white", size = 3)+
+  labs(x = "Day of week", y = "Count of TED Talks",
+       title = "Talks by published date (day of week)")+
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+
+#month
+
+TedRaw$published_date_month <- month(TedRaw$published_date_clean, label = TRUE)
+
+published_date_month <- TedRaw %>%
+  group_by(published_date_month) %>%
+  tally(name ="numTalks") 
+
+ggplot(data=published_date_month, aes(x=published_date_month, y=numTalks)) +
+  #geom_point(stat = "identity", aes(color = '#2b8cbe'))+
+  geom_bar(stat = "identity", fill = '#2b8cbe')+
+  geom_text(aes(label = numTalks), vjust = 1.6, color = "white", size = 3)+
+  labs(x = "Day of week", y = "Count of TED Talks",
+       title = "Talks by published date (month)")+
   theme(axis.text.x = element_text(angle = 45, hjust = 1))
 
